e4603e8a56aa001f02624dbaa810956215444ee8
TejavaV3 <mintmouse@naver.com>
2025-04-16 16:25:52 +0900

    0416

--- semi2/src/main/java/com/ksj/product/ProductDAO.java
+++ semi2/src/main/java/com/ksj/product/ProductDAO.java
@@ -49,6 +49,46 @@ public class ProductDAO {
 			} catch (Exception e2) {}
 		}
 	}
+	/*보이기 전체물품 찾기*/
+	public ArrayList<ProductDTO> stateProductList() {
+		try {
+			conn = com.ksj.db.ConnectionDB.getConn();
+			String sql = "select * from products where bbs_state = 0 order by create_date desc";
+			ps = conn.prepareStatement(sql);
+			ProductDTO dto = null;
+			rs = ps.executeQuery();
+			ArrayList<ProductDTO> arr = new ArrayList<ProductDTO>();
+			while(rs.next()) {
+				int products_id = rs.getInt("products_id");
+				int category_id = rs.getInt("category_id");
+				String buyer_id = rs.getString("buyer_id");
+				String seller_id = rs.getString("seller_id");
+				int price = rs.getInt("price");
+				String title = rs.getString("title");
+				String content =rs.getString("content");
+				String location = rs.getString("location");
+				int trade_state = rs.getInt("trade_state");
+				int bbs_state =rs.getInt("bbs_state");
+				String thumb_image = rs.getString("thumb_image");
+				int view_cnt = rs.getInt("view_cnt");
+				java.sql.Date create_date = rs.getDate("create_date");
+				java.sql.Date update_date = rs.getDate("update_date");
+				String image_uri = rs.getString("image_uri");
+				dto = new ProductDTO(products_id, category_id, buyer_id, seller_id, price, title, content, location, trade_state, bbs_state, thumb_image, view_cnt, create_date, update_date, image_uri);
+				arr.add(dto);
+			}
+			return arr;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}finally {
+			try {
+				if(rs!=null)rs.close();
+				if(ps!=null)ps.close();
+				if(conn!=null)conn.close();								
+			} catch (Exception e2) {}
+		}
+	}
 	/*검색물품 찾기*/
 	public ArrayList<ProductDTO> SearchProductList(String keyword, String sort) {
 		try {
